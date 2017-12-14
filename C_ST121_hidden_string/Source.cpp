89b6420326150f77274ed1bd361c773e4d46d3bd
justim <justim@gmail.com>
2017-12-14 09:28:15 +0800

    完成解題

--- C_ST121_hidden_string/Source.cpp
+++ C_ST121_hidden_string/Source.cpp
@@ -3,6 +3,33 @@
 using namespace std;
 int main()
 {
-
+	string source;
+	getline(cin, source);
+	int n;
+	cin >> n;
+	cin.ignore();
+	for (int i = 0; i < n; i++)
+	{
+		string target;
+		getline(cin, target);
+		int fpos = 0;
+		bool findall = true;
+		for (int j = 0; j < source.size(); j++)
+		{
+			fpos = target.find(source[j], fpos);
+			
+			if (fpos == string::npos)
+			{
+				findall = false;
+				break;
+			}
+			else
+				fpos++;
+		}
+		if (findall)
+			cout << "true" << endl;
+		else
+			cout << "false" << endl;
+	}
 	return 0;
 }
